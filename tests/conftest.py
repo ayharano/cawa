0201fb8b779dfe2be26d666591708ca29c5b0be4
Alexandre Harano <email@ayharano.dev>
2023-11-17 17:24:59 -0300

    chore: add database fixture as well as adjust client fixture
    We are using in-memory SQLite during the tests instead of a PostgreSQL instance

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,34 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
+from cawa.database import get_session
 from cawa.main import app
+from cawa.models import Base
 
 
 @pytest.fixture
-def client():
+def session():
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
+    Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    Base.metadata.create_all(engine)
+    yield Session()
+    Base.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def client(session):
+    def get_session_override():
+        return session
+
     with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
         yield client
+
+    app.dependency_overrides.clear()
