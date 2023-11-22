9ee6794f03e8092845715567e42b5746784015cc
Alexandre Harano <email@ayharano.dev>
2023-11-22 01:52:31 -0300

    feat: add User model with database tests

--- src/cawa/models.py
+++ src/cawa/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy_utils import Timestamp
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy_utils import Timestamp, generic_repr
 
 
 class Base(DeclarativeBase):
@@ -8,3 +9,16 @@ class Base(DeclarativeBase):
 
 class TimestampedBase(Base, Timestamp):
     __abstract__ = True
+
+
+@generic_repr
+class User(TimestampedBase):
+    __tablename__ = 'users'
+    __table_args__ = (
+        UniqueConstraint('username'),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    password: Mapped[str]
+    is_active: Mapped[bool]
---
+++ tests/test_models.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+import pytest
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+
+from cawa.models import User
+
+
+def test_create_and_retrieve_user(session):
+    before_new_user = datetime.utcnow()
+    new_user = User(
+        username='api-user',
+        password='VeryS@f&!',
+        is_active=True,
+    )
+    session.add(new_user)
+    session.commit()
+
+    user = session.scalar(
+        select(User)
+        .where(User.username == 'api-user')
+    )
+
+    assert user.username == 'api-user'
+    assert user.created > before_new_user
+    assert user.updated > user.created
+
+
+def test_cannot_create_duplicate_user(session):
+    new_user = User(
+        username='api-user',
+        password='VeryS@f&!',
+        is_active=True,
+    )
+    session.add(new_user)
+    session.commit()
+
+    new_user = User(
+        username='api-user',
+        password='duplicate',
+        is_active=True,
+    )
+    session.add(new_user)
+    with pytest.raises(IntegrityError):
+        session.commit()
